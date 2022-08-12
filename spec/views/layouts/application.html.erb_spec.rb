07b58b152c00ba9cf112a1bcb2bf3f111ea439d6
kazuyainoue <bauer24tbp@gmail.com>
2022-08-13 00:06:10 +0900

    rubocopを導入しました

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "layouts/application.html.erb", type: :view do
+RSpec.describe 'layouts/application.html.erb', type: :view do
   describe 'トップページへアクセス', type: :feature do
     before do
       visit root_path
@@ -8,11 +8,11 @@ RSpec.describe "layouts/application.html.erb", type: :view do
     it 'タイトルが表示されること' do
       expect(page).to have_link 'IT資格の口コミアプリ'
     end
-    
+
     it 'ログインリンクが表示されること' do
       expect(page).to have_link 'ログイン'
     end
-    
+
     it 'ユーザー登録リンクが表示されること' do
       expect(page).to have_link 'ユーザー登録'
     end
