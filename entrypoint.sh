0fa56a556743b377335372f630ca5c2856b50f7e
kazuyainoue <bauer24tbp@gmail.com>
2022-10-09 23:21:53 +0900

    config.ymlを修正しタイトルのびっくりマークを3つに変更しました#105

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 rm -f /review_app/tmp/pids/server.pid
 
+# ECS用に追記
+# bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
+
 exec "$@"
