aaa8c4b24e20199f1c61cf35ed5c39c3e2718cbe
kazuyainoue <bauer24tbp@gmail.com>
2022-10-12 22:19:27 +0900

    タイトルを変更しました#115

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def full_title(page_title = '')
-    base_title = 'IT資格の口コミアプリ'
+    base_title = 'シカクチコミ!'
     if page_title.empty?
       base_title
     else
