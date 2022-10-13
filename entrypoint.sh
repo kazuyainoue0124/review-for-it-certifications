a592971e4914c3f375cac18acb3789fae99b89ee
kazuyainoue <bauer24tbp@gmail.com>
2022-10-13 21:39:34 +0900

    entrypoint.shを元に戻しました#121

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 rm -f /review_app/tmp/pids/server.pid
 
 # ECS用に追記
-bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # bundle exec rails db:create RAILS_ENV=production
-# bundle exec rails db:migrate RAILS_ENV=production
-bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+
+# DBリセット&サンプルデータ投入用
+# bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rails db:seed RAILS_ENV=production
 
 exec "$@"
