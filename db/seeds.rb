d23242260fef6e573a2d7e24bb7db3e1633e77c2
kazuyainoue <bauer24tbp@gmail.com>
2022-10-13 18:53:44 +0900

    db/seeds.rbにサンプルデータを追加しました#121

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,18 +37,35 @@ Position.create!(
 Position.create!(
   name: 'アーキテクト'
 )
-
+Certificate.create!(
+  name: 'システムアーキテクト'
+)
+Certificate.create!(
+  name: '基本情報処理技術者'
+)
+Certificate.create!(
+  name: 'ITストラテジスト'
+)
+Certificate.create!(
+  name: '情報セキュリティアドミニストレータ'
+)
 Certificate.create!(
   name: 'ITパスポート'
 )
 Certificate.create!(
-  name: '基本情報技術者'
+  name: '情報セキュリティマネジメント'
 )
 Certificate.create!(
   name: '応用情報技術者'
 )
 Certificate.create!(
-  name: '情報処理安全保障支援士'
+  name: 'エンベッドシステムスペシャリスト'
+)
+Certificate.create!(
+  name: 'ITサービスマネージャ'
+)
+Certificate.create!(
+  name: '情報処理安全確保支援士'
 )
 Certificate.create!(
   name: 'データベーススペシャリスト'
@@ -57,39 +74,250 @@ Certificate.create!(
   name: 'ネットワークスペシャリスト'
 )
 Certificate.create!(
-  name: 'エンベデッドスペシャリスト'
+  name: 'プロジェクトマネージャ'
 )
 Certificate.create!(
-  name: 'ITストラテジスト'
+  name: '商業簿記1級'
 )
 Certificate.create!(
-  name: 'システムアーキテクト'
+  name: '商業簿記2級'
 )
 Certificate.create!(
-  name: 'LPIC'
+  name: '商業簿記3級'
 )
 Certificate.create!(
-  name: '簿記2級'
+  name: 'LPIC:レベル1'
 )
 Certificate.create!(
-  name: '簿記3級'
+  name: 'LPIC:レベル2'
 )
 Certificate.create!(
-  name: 'PMP'
+  name: 'LPIC:レベル3'
+)
+Certificate.create!(
+  name: 'AWS 認定クラウドプラクティショナー'
+)
+Certificate.create!(
+  name: 'AWS 認定ソリューションアーキテクト – アソシエイト'
+)
+Certificate.create!(
+  name: 'AWS 認定ソリューションアーキテクト – プロフェッショナル'
+)
+Certificate.create!(
+  name: 'AWS 認定デベロッパー – アソシエイト'
+)
+Certificate.create!(
+  name: 'AWS 認定 DevOps エンジニア – プロフェッショナル'
+)
+Certificate.create!(
+  name: 'AWS 認定システムオペレーション (SysOps) アドミニストレーター – アソシエイト'
+)
+Certificate.create!(
+  name: 'AWS 認定ビッグデータ – 専門知識'
+)
+Certificate.create!(
+  name: 'AWS 認定高度なネットワーク – 専門知識'
+)
+Certificate.create!(
+  name: 'AWS 認定 セキュリティ – 専門知識（SCS）'
+)
+Certificate.create!(
+  name: 'AWS 認定 機械学習 – 専門知識（MLS）'
+)
+Certificate.create!(
+  name: 'AWS 認定 Alexa スキルビルダー – 専門知識（AXS）'
+)
+Certificate.create!(
+  name: 'AWS 認定 データアナリティクス – 専門知識（DAS）'
+)
+Certificate.create!(
+  name: 'AWS 認定 データベース – 専門知識（DBS）'
+)
+Certificate.create!(
+  name: 'SAP認定ソリューションコンサルタント'
+)
+Certificate.create!(
+  name: 'SAP認定テクノロジーコンサルタント'
+)
+Certificate.create!(
+  name: 'SAP認定デベロップメントコンサルタント'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定アドミニストレーター'
+)
+Certificate.create!(
+  name: 'セールスフォース:上級アドミニストレーター'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定 Sales Cloud コンサルタント'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定 Service Cloud コンサルタント'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定デベロッパー'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定上級デベロッパー 一次試験'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定上級デベロッパー 二次試験'
+)
+Certificate.create!(
+  name: 'セールスフォース:認定テクニカルアーキテクト'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics AX 開発とインストール'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics AX 取引と物流'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics AX 財務管理'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics CRM 2016 Online Deployment'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics CRM 2016 インストール'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics CRM 2016 カスタマイズと構成'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics CRM 2016 営業管理'
+)
+Certificate.create!(
+  name: 'Microsoft Dynamics CRM 2016 顧客サービス'
+)
+Certificate.create!(
+  name: 'Oracle Certified Java Programmer  Silver '
+)
+Certificate.create!(
+  name: 'Oracle Certified Java Programmer  Gold '
+)
+Certificate.create!(
+  name: '中小企業診断士'
+)
+Certificate.create!(
+  name: 'ビジネス能力検定1級'
+)
+Certificate.create!(
+  name: 'ビジネス能力検定2級'
+)
+Certificate.create!(
+  name: 'ビジネス能力検定3級'
+)
+Certificate.create!(
+  name: '第一種電気工事士'
+)
+Certificate.create!(
+  name: '第一種電気主任技術者'
+)
+Certificate.create!(
+  name: '第三種電気主任技術者'
+)
+Certificate.create!(
+  name: '第二種電気工事士'
+)
+Certificate.create!(
+  name: '第二種電気主任技術者'
+)
+Certificate.create!(
+  name: 'G検定'
+)
+Certificate.create!(
+  name: 'E資格'
+)
+Certificate.create!(
+  name: 'BASIC級生産管理'
+)
+Certificate.create!(
+  name: '3級生産管理プランニング'
+)
+Certificate.create!(
+  name: '3級生産管理オペレーション'
+)
+Certificate.create!(
+  name: '2級生産管理プランニング(製品企画・設計管理)'
+)
+Certificate.create!(
+  name: '2級生産管理プランニング(加工型：組立型)'
+)
+Certificate.create!(
+  name: '2級生産管理プランニング(プロセス型)'
+)
+Certificate.create!(
+  name: '2級生産管理オペレーション(作業・工程・設備管理)'
+)
+Certificate.create!(
+  name: '2級生産管理オペレーション(購買・物流・在庫管理)'
 )
 
-10.times do |n|
-  User.create!(
-    name: "サンプルユーザー#{n + 1}",
-    email: "user#{n + 1}@sample.com",
-    password: 'password',
-    password_confirmation: 'password',
-    position_id: n + 1,
-    job_change_count: "#{n + 1}回",
-    salary_before_two_years: '351~400万円',
-    salary_before_one_years: '401~450万円',
-    current_salary: '451~500万円',
-    certificate_id: n + 1,
-    profile: "こんにちは！サンプルユーザー#{n + 1}です！"
-  )
-end
+# 10.times do |n|
+#   User.create!(
+#     name: "サンプルユーザー#{n + 1}",
+#     email: "user#{n + 1}@sample.com",
+#     password: 'password',
+#     password_confirmation: 'password',
+#     position_id: n + 1,
+#     job_change_count: "#{n + 1}回",
+#     salary_before_two_years: '351~400万円',
+#     salary_before_one_years: '401~450万円',
+#     current_salary: '451~500万円',
+#     certificate_id: n + 1,
+#     profile: "こんにちは！サンプルユーザー#{n + 1}です！"
+#   )
+# end
+
+User.create!(
+  name: "Kazuya",
+  email: "kazuya@example.com",
+  password: 'password',
+  password_confirmation: 'password',
+  position_id: 2,
+  job_change_count: "1回",
+  salary_before_two_years: '301~350万円',
+  salary_before_one_years: '351~400万円',
+  current_salary: '401~450万円',
+  certificate_id: 16,
+  profile: "こんにちは！Kazuyaです！"
+)
+User.create!(
+  name: "ゆうや",
+  email: "yuya@example.com",
+  password: 'password',
+  password_confirmation: 'password',
+  position_id: 2,
+  job_change_count: "1回",
+  salary_before_two_years: '401~450万円',
+  salary_before_one_years: '401~450万円',
+  current_salary: '351~400万円',
+  certificate_id: 2,
+  profile: "こんにちは！ゆうやです！"
+)
+
+Post.create!(
+  user_id: 1,
+  title: "【2週間】簿記3級に合格したので勉強法を解説！",
+  certificate_id: 16,
+  study_period: "~1ヶ月",
+  how_to_study: "「スッキリわかる日商簿記3級」という参考書を使って勉強しました。テキストと過去問題集の2冊とも購入し、テキストを通読後はひたすら問題演習をしたら合格できました。",
+  valuable_score: "すごく役に立った",
+  how_change: "会計業務の全体像を知るのに役立ちました。",
+  recommend_score: "すごくおすすめ",
+  recommended_person: "会計業務について学びたい人。会計の雰囲気を掴みたい人。",
+  comment: "簿記は3級なら簡単に受かるのでコスパが良いと思います！取るべき資格に迷ったらおすすめです！"
+)
+Post.create!(
+  user_id: 2,
+  title: "基本情報の資格を取得できたので勉強法をまとめます！",
+  certificate_id: 2,
+  study_period: "~3ヶ月",
+  how_to_study: "「キタミ式イラストIT塾 基本情報技術者」という参考書でインプットしつつ、「基本情報技術者過去問道場」というサイトで問題演習をしました。このサイトは無料で過去問が解き放題な上に、分野別に解いたりもできるので非常におすすめです。",
+  valuable_score: "どちらかと言うと役に立った",
+  how_change: "未経験でIT業界に転職してきたためITに関する知識が全くありませんでしたが、この資格を通じて得た知識によって、コンピュータやプログラムについてザックリと把握できたように思います。",
+  recommend_score: "どちらかと言うとおすすめ",
+  recommended_person: "未経験からIT業界に転職してきた人や、コンピュータサイエンスの全体像を大まかに知りたい人におすすめです。",
+  comment: "基本情報は過去問の焼き回しが多く、テスト対策のみでも合格できますが、どうせ取るならしっかりと中身を理解して受かった方がいいと思ってる派です！この資格にはそれだけの価値があった気がします！"
+)
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 rm -f /review_app/tmp/pids/server.pid
 
 # ECS用に追記
+bundle exec rails db:migrate:reset RAILS_ENV=production
 # bundle exec rails db:create RAILS_ENV=production
-bundle exec rails db:migrate RAILS_ENV=production
-# bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
 
 exec "$@"
